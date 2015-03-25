ca3fe78a7583b0c76e712d197087f40cfd89cbb5
Albert Pai <iamalbertpai@gmail.com>
2015-03-24 19:16:10 -0700

    initial commit

---
+++ code/app.js
@@ -0,0 +1,23 @@
+(function(){
+
+  function TestCtrl($templateCache, $scope) {
+    this.user = {name: 'Blake'};
+
+    console.log($templateCache.get('test.html'));
+  }
+
+  angular.module('app', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider.when('/', {
+      controller: 'TestCtrl as test',
+      templateUrl: 'test.html'
+    })
+    .otherwise('/');
+  })
+  .controller('TestCtrl', TestCtrl);
+
+  angular.module('app').run(function ($templateCache){
+    $templateCache.put('test.html', 'Hello {{ test.user.name }}!')
+  });
+
+})();
